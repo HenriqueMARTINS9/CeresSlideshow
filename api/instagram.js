bd8f37f17f516c6e754b8d4bd120de581265f9be
Henrique Martins <84198347+HenriqueMARTINS9@users.noreply.github.com>
2024-12-24 09:11:09 +0100

    Update instagram.js

--- api/instagram.js
+++ api/instagram.js
@@ -1,29 +1,16 @@
 export default async function handler(req, res) {
-    const accessToken = process.env.INSTAGRAM_GRAPH_API_ACCESS_TOKEN;
-    const userId = process.env.INSTAGRAM_USER_ID;
-    const mediaUrl = `https://graph.instagram.com/v17.0/${userId}/media?fields=id,media_type,media_url,caption&access_token=${accessToken}`;
-    const userUrl = `https://graph.instagram.com/${userId}?fields=username&access_token=${accessToken}`;
-    
-    try {
-        // Fetch username
-        const userResponse = await fetch(userUrl);
-        if (!userResponse.ok) {
-            throw new Error(`Error fetching username: ${userResponse.status} ${userResponse.statusText}`);
-        }
-        const userData = await userResponse.json();
-        const username = userData.username;
-
-        // Fetch media
-        const response = await fetch(mediaUrl);
-        if (!response.ok) {
-            throw new Error(`Error fetching media: ${response.status} ${response.statusText}`);
-        }
-        const data = await response.json();
-        
-        // Return username and media
-        res.status(200).json({ media: data.data, username: username });
-    } catch (error) {
-        console.error('API Error:', error.message);
-        res.status(500).json({ error: error.message });
+  const accessToken = process.env.INSTAGRAM_GRAPH_API_ACCESS_TOKEN;
+  const userId = process.env.INSTAGRAM_USER_ID;
+  const mediaUrl = `https://graph.instagram.com/v17.0/${userId}/media?fields=id,media_type,media_url,caption&access_token=${accessToken}`;
+  
+  try {
+    const response = await fetch(mediaUrl);
+    if (!response.ok) {
+      throw new Error(`Error fetching media: ${response.status} ${response.statusText}`);
     }
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
